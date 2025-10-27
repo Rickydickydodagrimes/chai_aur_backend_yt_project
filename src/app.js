011106771434a59a0f4a23f76bd9e4cd5e4f54f9
Rickydickydodagrimes <afrasiab0981@gmail.com>
2025-10-27 16:06:12 +0500

    added user and video model

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from express;
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+//CROSS ORIGIN RESOURCE SHARING is used to connect the frontend with backend bypassing cors policy (same origin policy)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+   credentials: true
+}))
+
+// some middleware or configs for accepting different types of data in different formats
+app.use(express.json({limit: "16kb"}));
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+
+app.use(express.static("public"));
+
+//cookie parser is used so server can access and manipulate cookies on clients browser
+app.use(cookieParser())
+
+export { app };
---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+
+    avatar: {
+        type: String, //using cloudinary url
+        required: true,    
+    },
+
+    coverImage: {
+        type: String, //using cloudinary url 
+    },
+
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true});
+
+//arrow function doesnt know the context i.e cant use this keyword, 
+// so we define the callback function with function keyword.
+userSchema.pre("save", async function (next) {    
+    if (!this.password.isModified("password")) return next();  //if password isnt modified or changed do not run the below code (to avoid multiple encryptions)
+
+   this.password = bcrypt.hash(this.password, 10);
+   next();
+});
+
+//making a custom method to match user-typed password with encypted password.
+userSchema.methods.isPasswordCorrect = async function (password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+            fullname : this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateAccessToken = function(){
+     return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
